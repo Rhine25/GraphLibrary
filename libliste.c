bd4f4ca3bd7180fcfbc0e2fe71540f2cf60c3cb6
Célia Dehors <celia.dehors@gmail.com>
2016-09-28 09:50:44 +0200

    First files

---
+++ libliste.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libliste.h"
+
+//creation
+struct list create(){
+    struct list *self;
+    self->first = NULL;
+    return *self;
+}
+
+//destruction
+void destroy(struct list *self){
+    
+}
+
+//ajout
+void add(struct list *self, int value){ //insertion en dbut de la liste doublement chainée
+    struct list_node *other = malloc(sizeof(struct list_node));
+    other->next = self->first;
+    self->first = other;
+    //TODO penser à gérer le previous
+}
+
+//suppression
+void del(struct list_node){
+    
+}
+
+//parcours en profondeur
+void dfs(const struct list){ //depth first search
+
+}
+
+//parcours en largeur
+void bfs(const struct list){ //breadth first search
+    
+}
+
+//vide
+int is_empty(const struct list){
+    
+}
+
+//taille
+size_t size(const struct list){
+    
+}
+
+//recherche
+struct list_node search(const struct list, int){
+    
+}
