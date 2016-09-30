d8ee046efdae60291db30ac7736307e028650f67
Vincent <vincent.durand25@gmail.com>
2016-09-30 20:44:04 +0200

    Update libgraphe.h

--- include/libgraphe.h
+++ include/libgraphe.h
@@ -1,5 +1,5 @@
-#ifndef LIBGRAPHE_H
-#define LIBGRAPHE_H
+#ifndef LIBGRAPH_H
+#define LIBGRAPH_H
 
 struct TypVoisins{
     int voisin;
@@ -13,5 +13,34 @@ struct TypGraph{
     int nbMaxSommets;
     TypVoisins* listesAdjacences;
 }
+
+//création du graphe avec un nombre de sommet demandé
+void create();
+
+//lecture d'un graphe dans un fichier texte
+void read();
+
+//inserer un nouveau sommet
+void addSommet(int s);
+
+//inserer une arete entre deux somets d'un graf
+void addArete(int src, int dest);
+
+//supprimer un sommet
+void delSommet(int s);
+
+//supprimer une arete entre deux somets d'un graf
+void delArete(int src, int dest);
+
+//afficher le graf dan le meme format que celui du fichier textee d'entrée
+void print();
+
+//enregistrer le graf dans un fichier texte
+void save();
+
+//quitter le programme et liberer toute la memoire
+void exitProg();
+
+
 
 #endif
